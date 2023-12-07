d9f5322d473bdb6307228c6c9d1d10bbf697f5ef
AJ7STONE <5100168@mypillar7uwc.ac.za>
2023-12-07 13:23:01 +0200

    first commit

---
+++ Assignment1/MilkSong.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class MilkSong
+    {
+        public static string Lyrics()
+        {
+            //int milk = 100;
+
+            for (int i = 99; i > 0; i--)
+            {
+                if (i >= 3)
+                {
+                    Console.WriteLine($"{i} bottle of milk on the wall, {i} bottles of milk");
+                    Console.WriteLine($"Take one down and pass it around, {i - 1} bottles of milk");
+                }
+                if (i == 1)
+                {
+                    Console.WriteLine($"{i} bottle of milk on the wall, {i} bottle of milk");
+                    break;
+                }
+
+                if (i == 2)
+                {
+                    Console.WriteLine($"{i} bottle of the milk on the wall, {i} bottles of milk");
+                    Console.WriteLine($"Take one down and pass it around, {i - 1} bottle of milk");
+                }
+                //milk--;
+            }
+            return "";
+        }
+    }
+}
---
+++ Assignment1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string milkies = MilkSong.Lyrics();
+            Console.WriteLine(milkies);
+        }
+    }
+}
